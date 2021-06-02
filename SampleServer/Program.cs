7bb694286682e50ca763e099445b5d6dd85980a8
Newp <mynewp@gmail.com>
2021-06-02 16:12:52 +0900

    샘플서버 파일 분리

--- SampleServer/Program.cs
+++ SampleServer/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System.Net;
 using System.Threading.Tasks;
-
-using Npgg.Socket;
 using System;
 using System.Net.Sockets;
 using System.Threading;
@@ -55,62 +53,4 @@ namespace ConsoleApp1
         public int SessionId { get; set; }
         public TcpClient Client { get; set; }
     }
-
-    public class SampleServer : AsyncServer<SampleSession>
-    {
-        public SampleServer() : base(4) { }
-
-        public override int GetPayloadLength(byte[] header) => BitConverter.ToInt32(header);
-
-        public override async Task OnReceiveMessage(SampleSession session, byte[] header, byte[] message, int length)
-        {
-            var cts = new CancellationTokenSource();
-            session.Client.ReceiveTimeout = 3000;
-            try
-            {
-                var stream = session.Client.GetStream();
-                Console.WriteLine("ready");
-                while (true)
-                {
-                    cts.CancelAfter(3000);
-                    var buffer = new byte[5];
-                    await stream.FillAsync(buffer, buffer.Length, cts.Token);
-                    
-                    Console.WriteLine($"{BitConverter.ToInt32(buffer, 0)} // {buffer[4]}");
-                }
-            }
-            catch (Exception)
-            {
-                Console.WriteLine($"Connected : {session.Client.Connected}");
-                Console.WriteLine("passed");
-            }
-            
-        }
-
-        public override SampleSession OnSessionOpened(TcpClient client)
-        {
-            int session = Interlocked.Increment(ref sessionIdSeq);
-            Console.WriteLine($"session opened : {session}");
-            return new SampleSession()
-            {
-                SessionId = session,
-                Client = client,
-            };
-        }
-
-        public override void OnSessionClosed(SampleSession session)
-        {
-        }
-
-        public override void OnSessionClosed(SampleSession session, Exception ex)
-        {
-            Console.WriteLine($"error> {session.SessionId}\n {ex.ToString()}");
-        }
-
-        int sessionIdSeq = 0;
-    }
 }
-
-
- 
-
